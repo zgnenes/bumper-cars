800ef8d4bf8bc00bd18228067a18cd43cc7ef26a
Squirex <48452405+sukireks@users.noreply.github.com>
2020-12-20 15:52:11 +0300

    Add files via upload

---
+++ esx_vehiclesRent/config.lua
@@ -0,0 +1,29 @@
+Config = {}
+
+Config.PlateText = '000000'
+
+Config.MaxNoJob = 1 
+Config.MaxJob = 3 
+
+Config.DistanseSpawn = 50.0 
+Config.DistanseLock = 5.0 
+
+Config.CarLock = true 
+
+Config.VehiclePositions = {            
+    
+    {x = -1735.68, y = -1124.84, z = 13.0, h = 134.24, model = 'lambokart', price = 3, job = 'none'},
+    {x = -1739.48, y = -1129.96, z = 13.0, h = 134.24, model = 'lambokart', price = 3, job = 'none'},
+    {x = -1741.96, y = -1132.8, z = 13.0, h = 134.24, model = 'lambokart', price = 3, job = 'none'},
+    {x = -1744.6, y = -1136.04, z = 13.0, h = 134.24, model = 'lambokart', price = 3, job = 'none'},
+    {x = -1749.03, y = -1141.01, z = 13.0, h = 134.24, model = 'lambokart', price = 3, job = 'none'},
+    {x = -1751.63, y = -1143.98, z = 13.0, h = 134.24, model = 'lambokart', price = 3, job = 'none'},
+    {x = -1754.24, y = -1147.22, z = 13.0, h = 134.24, model = 'lambokart', price = 3, job = 'none'},
+    {x = -1756.35, y = -1149.39, z = 13.0, h = 134.24, model = 'lambokart', price = 3, job = 'none'},
+    {x = -1759.10, y = -1152.77, z = 13.0, h = 134.24, model = 'lambokart', price = 3, job = 'none'},
+
+
+
+
+}	
+
